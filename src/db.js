96f4abe3df4502ea98d78b8d4ed294b18b1b8f10
ghina <baassiri.ghina@gmail.com>
2024-09-13 21:15:24 +0300

    Remove redundant let mongoClient declarations

--- src/db.js
+++ src/db.js
@@ -1,90 +1,89 @@
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
 let mongoClient;
 
-async function connectToDB(uri){
+async function connectToDB(uri) {
     try {
-        mongoClient = new MongoClient(uri);
-        await mongoClient.connect();
-        console.log('Connection to MongoDB Succeeded.')
+        if (!mongoClient) {
+            mongoClient = new MongoClient(uri);
+            await mongoClient.connect();
+            console.log('Connection to MongoDB Succeeded.');
+        }
         return mongoClient;
     } catch (error) {
-        console.error('Connection to MongoDB Failed.')
-        return 'Connection to MongoDB Failed.';
-    }  
+        console.error('Connection to MongoDB Failed.', error);
+        throw new Error('Connection to MongoDB Failed.');
+    }
 }
 
 // Add student
-export async function addStudent(studentObj)
-{
+export async function addStudent(studentObj) {
     const uri = process.env.DB_URI;
-    let mongoClient;
-    mongoClient = await connectToDB(uri);
-    const db =  mongoClient.db('school');
-    const collection = db.collection('students');
     try {
+        const mongoClient = await connectToDB(uri);
+        const db = mongoClient.db('school');
+        const collection = db.collection('students');
         await collection.insertOne(studentObj);
+        return 'Student added successfully.';
     } catch (error) {
+        console.error(error);
         return 'Failed to add student.';
     }
-    
 }
 
-// Get student by name 
-export async function getAllStudents(){
+// Get all students
+export async function getAllStudents() {
     const uri = process.env.DB_URI;
-    let mongoClient;
-    mongoClient = await connectToDB(uri);
-    const db =  mongoClient.db('school');
-    const collection = db.collection('students');
     try {
-        return collection.find({}).toArray();
+        const mongoClient = await connectToDB(uri);
+        const db = mongoClient.db('school');
+        const collection = db.collection('students');
+        return await collection.find({}).toArray();
     } catch (error) {
+        console.error(error);
         return 'Failed to get students.';
     }
 }
 
-// Get student by name 
-export async function getStudentsByName(name){
+// Get student by name
+export async function getStudentsByName(name) {
     const uri = process.env.DB_URI;
-    let mongoClient;
-    mongoClient = await connectToDB(uri);
-    const db =  mongoClient.db('school');
-    const collection = db.collection('students');
     try {
-        return collection.find({name}).toArray();
+        const mongoClient = await connectToDB(uri);
+        const db = mongoClient.db('school');
+        const collection = db.collection('students');
+        return await collection.find({ name }).toArray();
     } catch (error) {
+        console.error(error);
         return 'Failed to get filtered students.';
     }
 }
 
 // Update student
-export async function updateStudentByName( name, updateFields){
+export async function updateStudentByName(name, updateFields) {
     const uri = process.env.DB_URI;
-    let mongoClient;
-    mongoClient = await connectToDB(uri);
-    const db =  mongoClient.db('school');
-    const collection = db.collection('students');
     try {
-        await collection.updateMany(
-            {name},
-            {$set:updateFields}
-        )
+        const mongoClient = await connectToDB(uri);
+        const db = mongoClient.db('school');
+        const collection = db.collection('students');
+        await collection.updateMany({ name }, { $set: updateFields });
+        return 'Student updated successfully.';
     } catch (error) {
+        console.error(error);
         return 'Failed to update student.';
     }
-    
 }
 
 // Delete student
-export async function deleteStudentByName(name){
+export async function deleteStudentByName(name) {
     const uri = process.env.DB_URI;
-    let mongoClient;
-    mongoClient = await connectToDB(uri);
-    const db =  mongoClient.db('school');
-    const collection = db.collection('students');
     try {
-        await collection.deleteOne({name})
+        const mongoClient = await connectToDB(uri);
+        const db = mongoClient.db('school');
+        const collection = db.collection('students');
+        await collection.deleteOne({ name });
+        return 'Student deleted successfully.';
     } catch (error) {
+        console.error(error);
         return 'Failed to delete student.';
     }
 }
